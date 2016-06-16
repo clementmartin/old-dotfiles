978b84f9cee9e0ea79177239f1f5923004863860
Clement Martin <clement.martin@devoteam.com>
2016-06-16 10:55:30 +0200

    Initial import

---
+++ deploy_dotfiles.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+warn_and_exit() {
+  echo
+  echo "!!!!!!!!!!!!!WARNING WARNING WARNING!!!!!!!!!!!!!!!"
+  echo "THIS WILL OVERWRITE ALL SUPPORTED CONFIG FILES !!!!"
+  echo "..PLEASE USE \"-YES-RESET-MY-CONF\" to really do it.."
+  echo
+  exit 1
+}
+
+overwrite_config() {
+  for dir in *; do
+    if [[ -d "${dir}" ]]; then
+      for item in "${dir}"/*; do
+        cp -vr "${item}" "${HOME}/.${item##*/}"
+      done
+    fi
+  done
+  exit 0
+}
+
+[[ $1 = "-YES-RESET-MY-CONF" ]] || warn_and_exit
+
+overwrite_config
